87fb1cd6a16baba4b0eef26a2922e5d44f2cddd1
z-ko <peter7661211@gmail.com>
2014-12-14 19:13:53 -0500

    Respond to server pings

--- botmain.py
+++ botmain.py
@@ -9,7 +9,7 @@ botnick = "botnick"
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 ircsock.connect((server, 6667)) #connect to server using 6667
 ircsock.send(u'NICK %s\n' % (botnick))#user auth
-ircsock.send(u'USER %s %s :%s\n' % (botnick, server, botnick)) 
+ircsock.send(u'USER %s bla %s :%s\n' % (botnick, server, botnick)) 
 
 def ping():
 	ircsock.send("PONG :pong\n")
@@ -26,4 +26,6 @@ joinchan(channel)
 while True:	
 	ircmsg = ircsock.recv(2048)
 	ircmsg = ircmsg.strip('\n\r')
-	
+	print(ircmsg)
+	if ircmsg.find("PING :") != -1:	#respond to server pings
+		ping()
